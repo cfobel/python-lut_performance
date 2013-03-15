e1c37a4ec40fb0eae17d5ce305017dd802ae9acc
Christian Fobel <christian@fobel.net>
2013-03-15 18:37:59 -0400

    Add load_factor, show runtime to create LUT
    Run test for `[1, 2, 4, 8, 16] * run_count`, where `run_count = 10000`.
    
    Results:
    
        Runtime to create lookup table: 1.88234186172 seconds
    
        Python runtimes:
            lut: 1.217947 seconds, calc: 2.211026 seconds
    
        Cython runtimes:
            [load factor   1] lut: 0.623400986195 seconds, calc: 0.722425997257 seconds
            [load factor   2] lut: 1.24354505539 seconds, calc: 1.44377803802 seconds
            [load factor   4] lut: 2.49118304253 seconds, calc: 2.89230394363 seconds
            [load factor   8] lut: 4.96970081329 seconds, calc: 5.77595090866 seconds
            [load factor  16] lut: 9.9788980484 seconds, calc: 11.5706911087 seconds
    
    Test system:
    
          Model Name: Intel(R) Core(TM) i7-3517U CPU @ 1.90GHz
             Cpu Mhz: 1900.000
          Cache Size: 4096 KB
            Bogomips: 4789.15

--- lut_performance/lut_performance.py
+++ lut_performance/lut_performance.py
@@ -7,9 +7,8 @@ import numpy as np
 from cLut_performance import run_time_test as cRun_time_test
 
 
-def run_time_test():
+def run_time_test(run_count=10000, load_factor=1):
     runtimes = OrderedDict()
-    run_count = 10000
     repeat = 3
 
     d = dict(zip([(x, y) for x in range(34) for y in range(34)], range(34 * 34)))
@@ -22,7 +21,7 @@ def run_time_test():
     for r in range(repeat):
         total = 0
         start = datetime.now()
-        for run_count in range(10000):
+        for i in range(run_count * load_factor):
             for p in block_positions: total += d[p]
         end = datetime.now()
         runtimes.setdefault('lut', {})[r] = (end - start).total_seconds()
@@ -30,7 +29,7 @@ def run_time_test():
     for r in range(repeat):
         total = 0
         start = datetime.now()
-        for run_count in range(10000):
+        for i in range(run_count * load_factor):
             for p in block_positions: total += p[0] * 34 + p[1]
         end = datetime.now()
         runtimes.setdefault('calc', {})[r] = (end - start).total_seconds()
@@ -38,3 +37,25 @@ def run_time_test():
     runtimes['lut'] = min(runtimes['lut'].values())
     runtimes['calc'] = min(runtimes['calc'].values())
     return runtimes
+
+
+def main():
+    print 'Runtimes'
+    run_count = 10000
+    print '  Python:', ', '.join(['%s: %s seconds' % (k, v)
+                                  for k, v in run_time_test(run_count).items()])
+
+    run_time = min(timeit.repeat(
+        'd = dict(zip([(x, y) for x in range(34) '\
+        'for y in range(34)], range(34 * 34)))', number=run_count))
+    print 'Runtime to create lookup table:', run_time
+
+    results = OrderedDict([(1 << r, cRun_time_test(run_count, 1 << r).items())
+                           for r in range(5)])
+    result_strs = [(label, ', '.join(['%s: %s seconds' % (k, v) for k, v in r]))
+                   for label, r in results.iteritems()]
+    print '\n'.join(['    [load factor %3d] %s' % item for item in result_strs])
+
+
+if __name__ == '__main__':
+    main()
--- src/cLut_performance.pyx
+++ src/cLut_performance.pyx
@@ -1,38 +1,57 @@
 from datetime import datetime
 from collections import OrderedDict
-import timeit
 
 import numpy as np
 
 
-def run_time_test():
+cdef inline lookup_0(d, block_positions):
+    cdef int total = 0
+    for p in block_positions:
+        total += d[p]
+
+
+cdef inline calc_0(d, block_positions):
+    cdef int total = 0
+    for p in block_positions:
+        total += p[0] * 34 + p[1]
+
+
+cdef inline float my_timeit(f, d, block_positions, int repeat, int run_count):
+    cdef int r
+    cdef int i
+
+    runtimes = np.empty(repeat, dtype=float)
+    for r in range(repeat):
+        total = 0
+        start = datetime.now()
+        for i in range(run_count):
+            if f == 'calc_0':
+                calc_0(d, block_positions)
+            elif f == 'lookup_0':
+                lookup_0(d, block_positions)
+            else:
+                raise ValueError, 'Unknown f: %s' % f
+        end = datetime.now()
+        runtimes[r] = (end - start).total_seconds()
+    return runtimes.min()
+
+
+def run_time_test(run_count=10000, load_factor=1):
+    cdef int total
+    cdef int repeat = 3
+    cdef int i
+
     runtimes = OrderedDict()
-    run_count = 10000
-    repeat = 3
 
     d = dict(zip([(x, y) for x in range(34) for y in range(34)], range(34 * 34)))
+
     positions = d.keys()
     block_positions = positions[:]
     rand_state = np.random.RandomState()
     rand_state.shuffle(block_positions)
-    total = 0
 
-    for r in range(repeat):
-        total = 0
-        start = datetime.now()
-        for run_count in range(10000):
-            for p in block_positions: total += d[p]
-        end = datetime.now()
-        runtimes.setdefault('lut', {})[r] = (end - start).total_seconds()
-
-    for r in range(repeat):
-        total = 0
-        start = datetime.now()
-        for run_count in range(10000):
-            for p in block_positions: total += p[0] * 34 + p[1]
-        end = datetime.now()
-        runtimes.setdefault('calc', {})[r] = (end - start).total_seconds()
-
-    runtimes['lut'] = min(runtimes['lut'].values())
-    runtimes['calc'] = min(runtimes['calc'].values())
+    runtimes['lut'] = my_timeit('lookup_0', d, block_positions, repeat,
+                                load_factor * run_count)
+    runtimes['calc'] = my_timeit('calc_0', d, block_positions, repeat,
+                                 load_factor * run_count)
     return runtimes
